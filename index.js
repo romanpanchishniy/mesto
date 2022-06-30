7a9513766c0ec4fc334dd0387906c10a74efe435
Panchischniy Roman <romanpanchishniy@yandex.ru>
2022-06-30 23:09:39 +0300

    Final: перед проверкой системы Практикума

---
+++ index.js
@@ -0,0 +1,33 @@
+const popup = document.querySelector (".popup");
+const openPopupButton = document.querySelector (".profile__edit-button");
+const closePopupButton = document.querySelector (".popup__close-icon");
+const popupSubmitButton = document.querySelector(".popup__input");
+
+const profileName = document.querySelector(".profile__name");
+const profileProfession = document.querySelector(".profile__profession");
+
+let popupName = document.querySelector(".popup__name");
+let popupProfession = document.querySelector(".popup__profession");
+
+
+const openPopup = function () {
+    popup.classList.add("popup_opened");
+    popupName.value = profileName.textContent;
+    popupProfession.value = profileProfession.textContent;
+};
+
+const closePopup = function () {
+    popup.classList.remove("popup_opened");
+}
+
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = popupName.value;
+    profileProfession.textContent = popupProfession.value;
+    closePopup();
+};
+
+openPopupButton.addEventListener("click", openPopup);
+closePopupButton.addEventListener("click", closePopup);
+popupSubmitButton.addEventListener('submit', formSubmitHandler);
